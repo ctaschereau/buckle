d89b2baa73fd90137719183e202453fae89b23a1
Cedric Taschereau <cedric.taschereau@gmail.com>
2013-12-24 09:09:14 -0500

    changed error handling 2

--- server.js
+++ server.js
@@ -38,7 +38,9 @@ app.get('/addBuckle', dev_routes.addBuckleForm);
 app.post('/addBuckle', dev_routes.addBuckle);
 
 process.on('uncaughtException', function(err) {
-	console.log('Caught exception: ' + JSON.stringify(err));
+	console.error((new Date).toUTCString() + ' uncaughtException:', err.message)
+	console.error(err.stack)
+	process.exit(1)
 });
 
 http.createServer(app).listen(app.get('port'), function(){
