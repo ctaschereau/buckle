9d179720fee675fcf99960d184089c5538976e32
Cedric Taschereau <cedric.taschereau@gmail.com>
2013-12-25 10:08:03 -0500

    Better logging around getImage

--- server/routes/index.js
+++ server/routes/index.js
@@ -29,34 +29,38 @@ exports.getImage = function(req, res) {
 	var imagePath = root_dir + '/upload/' + image_name;
 	var thumbnailPath = root_dir + '/upload/thumbnails/' + image_name;
 	
-	var writeImageToResponse = function(imageData){
+	var writeImageToResponse = function(err, imageData){
+		if(err) {
+			logger.error('getImage error ! ' + err.message);
+			logger.error(err);
+			res.writeHead(500);
+			res.write(err.message);
+			res.end();
+			return;
+		}
 		res.writeHead(200, {"Content-Type": 'image/jpg'});
 		res.write(imageData);
 		res.end();
 	};
 	
-	//logger.trace('Going to serve request for ' + (is_thumbnail ? 'thumbnail ' : 'image ') + (is_thumbnail ? thumbnailPath : imagePath));
+	logger.debug('Going to serve request for ' + (is_thumbnail ? 'thumbnail ' : 'image ') + (is_thumbnail ? thumbnailPath : imagePath));
 	
 	fs.stat((is_thumbnail ? thumbnailPath : imagePath), function(err, stat) {
 		// if the file does not exist
 		if(err && err.code == 'ENOENT') {
 			// if it is not a thumbnail, display empty image. Otherwise, create the thumbnail
 			if(!is_thumbnail) {
-				fs.readFile(root_dir + '/public/images/noImage.jpg', function (err, data) {
-					writeImageToResponse(data);
-				});
+				logger.debug('Image does not exist, going to display the default image instead ...');
+				fs.readFile(root_dir + '/public/images/noImage.jpg', writeImageToResponse);
 			} else {
-				imageUtils.createThumbnail(imagePath, thumbnailPath, function(err, data){
-					writeImageToResponse(data);
-				});
+				logger.debug('Thumbnail does not exist, going to create it ...');
+				imageUtils.createThumbnail(imagePath, thumbnailPath, writeImageToResponse);
 			}
 		} else if(err) {
-			throw err;
+			writeImageToResponse(err);
 		} else {
 			// File exists, so read it and return it
-			fs.readFile((is_thumbnail ? thumbnailPath : imagePath), function(err, data){
-				writeImageToResponse(data);
-			});
+			fs.readFile((is_thumbnail ? thumbnailPath : imagePath), writeImageToResponse);
 		}
 	});
 };
